87299c3e24d7747118b352a446bdc7b50f07696b
ShawnaZhou <2359871499@qq.com>
2022-01-17 15:15:26 +0800

    update v2

--- assets/Script/MainController.ts
+++ assets/Script/MainController.ts
@@ -2,45 +2,17 @@ const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class MainController extends cc.Component {
-  //随机植物1
-  @property(cc.Prefab)
-  plant1: cc.Prefab = null;
+  //仙人掌预制件数组
+  @property([cc.Prefab])
+  plantsArray: cc.Prefab[] = new Array();
 
-  //随机植物2
-  @property(cc.Prefab)
-  plant2: cc.Prefab = null;
+  //地面数组
+  @property([cc.Node])
+  grounds: cc.Node[] = new Array();
 
-  //随机植物3
-  @property(cc.Prefab)
-  plant3: cc.Prefab = null;
-
-  //随机植物4
-  @property(cc.Prefab)
-  plant4: cc.Prefab = null;
-
-  //随机植物5
-  @property(cc.Prefab)
-  plant5: cc.Prefab = null;
-
-  //随机植物6
-  @property(cc.Prefab)
-  plant6: cc.Prefab = null;
-
-  //地面1
-  @property(cc.Node)
-  ground1: cc.Node = null;
-
-  //地面2
-  @property(cc.Node)
-  ground2: cc.Node = null;
-
-  //高处云
-  @property(cc.Node)
-  cloudHigher: cc.Node = null;
-
-  //低处云
-  @property(cc.Node)
-  cloudLower: cc.Node = null;
+  //云数组
+  @property([cc.Node])
+  clouds: cc.Node[] = new Array();
 
   //当前分数节点
   @property(cc.Node)
@@ -66,25 +38,27 @@ export default class MainController extends cc.Component {
   @property(cc.Button)
   resetBtn: cc.Button = null;
 
+  //跳跃高度
+  @property
+  jumpHeight: number = 150;
+
+  //云移动速度
+  @property
+  cloudSpeed: number = 2;
+
+  //路移动速度
+  @property
+  roadSpeed: number = 5;
+
   //play状态，初始化为false
   playStatus: boolean = false;
   //存活状态，初始化为true
   lifeStatus: boolean = true;
   //历史最高分数
   historyRecord: number = 0;
-  // 定时器Id
-  timer: number = 0;
-  // 是否正在跳跃:
-  jumpStatus: boolean = false;
   //跳跃动画
   JumpAnimate: cc.Tween = null;
-  //跳跃高度
-  jumpHeight: number = 150;
-  //云移动速度
-  cloudSpeed: number = 2;
-  //路移动速度
-  roadSpeed: number = 5;
-  //声明一个trex移动对象，控制其所有移动方法
+  //声明一个trex移动类，控制其所有移动方法
   moveAction: any = null;
   // throttle 参数
   valid: boolean = true;
@@ -96,71 +70,64 @@ export default class MainController extends cc.Component {
   score: number = 0;
   //是否允许闪烁
   blinkVaild: boolean = true;
+  //是否初始化
+  initial: boolean = true;
 
   onLoad() {
     cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.throttle, this);
-    this.instiatePrefab(2);
+    this.instiatePrefab(this.grounds[1]);
     this.resetBtn.node.on("click", this.restart, this);
   }
 
   start() {
     let manager = cc.director.getCollisionManager();
     manager.enabled = true;
-    manager.enabledDebugDraw = true;
-    manager.enabledDrawBoundingBox = true;
-    this.moveAction = {
-      animateTrexComponent: this.node.getComponent(cc.Animation)
-        ? this.node.getComponent(cc.Animation)
-        : null,
-      //animateTrexStart
-      trexStart: function () {
-        return this.animateTrexComponent.play("Move");
-      },
-      //animateTrexPause
-      trexJump: function () {
-        return this.animateTrexComponent.play("Jump");
-      },
-      trexDie: function () {
-        return this.animateTrexComponent.play("Dead");
-      },
-      trexPause: function () {
-        return this.animateTrexComponent.pause();
-      },
+
+    class trexMove {
+      animateComponent: cc.Animation;
+
+      constructor(anim: cc.Animation) {
+        this.animateComponent = anim;
+      }
+      trexStart = () => {
+        return this.animateComponent.play("Move");
+      };
+      trexJump = () => {
+        return this.animateComponent.play("Jump");
+      };
+      trexDie = () => {
+        return this.animateComponent.play("Dead");
+      };
+      trexPause = () => {
+        return this.animateComponent.pause();
+      };
       //animateStop
-      trexStop: function () {
-        return this.animateTrexComponent.stop();
-      },
-    };
+      trexStop = () => {
+        return this.animateComponent.stop();
+      };
+    }
+    this.moveAction = new trexMove(this.node.getComponent(cc.Animation));
   }
 
   //  节流 Done
   throttle(event: cc.Event.EventKeyboard) {
     if (!this.valid) {
       return;
-    } else {
-      this.onKeyDown(event);
     }
     this.valid = false;
-    this.timer = setTimeout(() => {
-      this.valid = true;
-    }, 600);
+    this.onKeyDown(event);
   }
 
   //监听键盘事件，按空格则进行逻辑处理。 Done
   onKeyDown(event: cc.Event.EventKeyboard) {
-    if (this.jumpStatus) {
-      return;
-    }
     if (event.keyCode === cc.macro.KEY.space) {
       if (!this.playStatus && this.lifeStatus) {
         this.playStatus = true;
         this.moveAction.trexStart();
-        this.jumpStatus = true;
         let tempAnimate = this.currentNode.getComponent(cc.Animation);
         tempAnimate.play();
         this.jumpAction();
       } else if (this.playStatus && this.lifeStatus) {
-        this.jumpStatus = true;
         this.jumpAction();
       } else if (this.playStatus && !this.lifeStatus) {
         return;
@@ -170,20 +137,14 @@ export default class MainController extends cc.Component {
     }
   }
 
-  //跳跃，异步处理。 Done
-  async jumpAction() {
+  //跳跃处理。 Done
+  jumpAction() {
     this.moveAction.trexJump();
-    await this.jumpAnimate().then(() => {
-      let tempTimer = setTimeout(() => {
-        this.jumpStatus = false;
-        if (this.lifeStatus) this.moveAction.trexStart();
-        clearTimeout(tempTimer);
-      }, 600);
-    });
+    this.jumpAnimate();
   }
 
   //跳跃动画 Done
-  async jumpAnimate() {
+  jumpAnimate() {
     this.JumpAnimate = cc
       .tween(this.node)
       .by(
@@ -196,6 +157,10 @@ export default class MainController extends cc.Component {
         { position: cc.v3(0, -this.jumpHeight, 0) },
         { easing: "quadIn" }
       )
+      .call(() => {
+        this.valid = true;
+        this.moveAction.trexStart();
+      })
       .start();
   }
 
@@ -205,25 +170,17 @@ export default class MainController extends cc.Component {
   }
 
   //实例化预制件
-  instiatePrefab(e: number) {
-    if (e == 1) {
-      this.ground1.removeAllChildren();
-    } else if (e == 2) {
-      this.ground2.removeAllChildren();
+  instiatePrefab(ground: cc.Node) {
+    if (ground.children.length > 0) {
+      ground.removeAllChildren();
     }
-    let tempArray = [
-      this.plant1,
-      this.plant2,
-      this.plant3,
-      this.plant4,
-      this.plant5,
-      this.plant6,
-    ];
-
     let randomNums = Math.floor(Math.random() * (6 - 3)) + 3;
     for (let i = 0; i < randomNums; i++) {
-      let tempNode = cc.instantiate(tempArray[Math.floor(Math.random() * 6)]);
-      tempNode.parent = e == 1 ? this.ground1 : this.ground2;
+      let tempRandom = Math.floor(
+        !this.initial ? Math.random() * 6 : Math.random() * 4
+      );
+      let tempNode = cc.instantiate(this.plantsArray[tempRandom]);
+      tempNode.parent = ground ? ground : this.grounds[0];
       tempNode.setPosition(
         -(
           (2400 / randomNums) * (i + 1) +
@@ -232,10 +189,11 @@ export default class MainController extends cc.Component {
         0
       );
     }
+    this.initial = false;
   }
 
   //碰撞处理
-  async onCollisionEnter(other: cc.Node, self: cc.Node) {
+  onCollisionEnter(other: cc.Node, self: cc.Node) {
     this.jumpAnimateStop();
     this.moveAction.trexStop();
     this.moveAction.trexDie();
@@ -254,48 +212,53 @@ export default class MainController extends cc.Component {
       tempAnimate2.pause();
       tempAnimate2.setCurrentTime(this.historyRecord);
     }
+    let timer = setTimeout(() => {
+      this.valid = true;
+      clearTimeout(timer);
+    }, 300);
   }
 
   //重新开始游戏事件
   restart() {
+    this.valid = true;
     this.playStatus = true;
     this.lifeStatus = true;
-    this.ground1.removeAllChildren();
-    this.ground2.removeAllChildren();
+    this.grounds[0].removeAllChildren();
+    this.grounds[1].removeAllChildren();
     let tempAnimate = this.currentNode.getComponent(cc.Animation);
     tempAnimate.play();
     this.resetBtn.node.y = -500;
     this.node.y = -270;
     this.moveAction.trexStart();
+    this.initial = true;
   }
 
-  protected onDestroy(): void {
-    clearInterval(this.timer != 0 ? this.timer : null);
-  }
-
-  update(dt) {
+  update(dt: number) {
     if (this.playStatus && this.lifeStatus) {
-      this.cloudHigher.x -= this.cloudSpeed;
-      this.cloudLower.x -= this.cloudSpeed;
-      this.ground1.x -= this.roadSpeed;
-      this.ground2.x -= this.roadSpeed;
-    }
-    if (this.ground1.x <= -480) {
-      this.ground1.setPosition(this.ground1.x + 4800, this.ground1.y);
-      this.instiatePrefab(1);
-    }
-    if (this.ground2.x <= -480) {
-      this.ground2.setPosition(this.ground2.x + 4800, this.ground1.y);
-      this.instiatePrefab(2);
-    }
-    if (this.cloudHigher.x <= -600) {
-      this.cloudHigher.setPosition(
-        this.cloudHigher.x + 1150,
-        this.cloudHigher.y
-      );
-    }
-    if (this.cloudLower.x <= -680) {
-      this.cloudLower.setPosition(this.cloudLower.x + 1200, this.cloudLower.y);
+      this.clouds[0].x -= this.cloudSpeed;
+      this.clouds[1].x -= this.cloudSpeed;
+      this.grounds[0].x -= this.roadSpeed;
+      this.grounds[1].x -= this.roadSpeed;
+      if (this.grounds[0].x <= -480) {
+        this.grounds[0].setPosition(
+          this.grounds[0].x + 4800,
+          this.grounds[0].y
+        );
+        this.instiatePrefab(this.grounds[0]);
+      }
+      if (this.grounds[1].x <= -480) {
+        this.grounds[1].setPosition(
+          this.grounds[1].x + 4800,
+          this.grounds[0].y
+        );
+        this.instiatePrefab(this.grounds[1]);
+      }
+      if (this.clouds[0].x <= -600) {
+        this.clouds[0].setPosition(this.clouds[0].x + 1150, this.clouds[0].y);
+      }
+      if (this.clouds[1].x <= -680) {
+        this.clouds[1].setPosition(this.clouds[1].x + 1200, this.clouds[1].y);
+      }
     }
   }
 }
